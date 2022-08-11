a258288717c547992759001ae7255265a959731e
Raul <raulvsh@gmail.com>
2022-08-11 16:44:02 +0200

    Car Factory

---
+++ matrixes/src/main.rs
@@ -0,0 +1,13 @@
+
+
+fn main(){
+    // Declare array, initialize all values, compiler infers length = 7
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    // Declare array, initialize all values to 0, length = 5
+    let bytes = [0; 5];
+
+    
+
+    println!("{}",days[0]);
+}
