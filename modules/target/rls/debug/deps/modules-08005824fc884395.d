cfdff9fa8c83a8bdc9725d9e6db8214e56e43ec4
Raul <raulvsh@gmail.com>
2022-08-23 12:35:33 +0200

    modules example text_processing

---
+++ modules/target/rls/debug/deps/modules-08005824fc884395.d
@@ -0,0 +1,5 @@
+c:\Users\Raúl\rust-learning-path\modules\target\rls\debug\deps\modules-08005824fc884395.rmeta: src\main.rs
+
+c:\Users\Raúl\rust-learning-path\modules\target\rls\debug\deps\modules-08005824fc884395.d: src\main.rs
+
+src\main.rs:
---
+++ modules/target/rls/debug/deps/modules-7e83ab875ba5a676.d
@@ -0,0 +1,5 @@
+c:\Users\Raúl\rust-learning-path\modules\target\rls\debug\deps\modules-7e83ab875ba5a676.rmeta: src\main.rs
+
+c:\Users\Raúl\rust-learning-path\modules\target\rls\debug\deps\modules-7e83ab875ba5a676.d: src\main.rs
+
+src\main.rs:
